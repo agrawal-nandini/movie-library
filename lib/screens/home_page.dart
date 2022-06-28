7b9110e88b8af3618f48fb2c98bd030d0b72193b
agrawal-nandini <95284734+agrawal-nandini@users.noreply.github.com>
2022-06-29 00:06:09 +0530

    Categories Added

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -15,6 +15,9 @@ class _HomePageState extends State<HomePage> {
   List trendingMovies = [];
   List topRatedMovies = [];
   List tv = [];
+  List tvAir = [];
+  List movieDiscover = [];
+  List movieUpcoming = [];
   final String apiKey = '3b6715502c069651f4ed7730f2d9d9b5';
   final String readAccessToken =
       'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYjY3MTU1MDJjMDY5NjUxZjRlZDc3MzBmMmQ5ZDliNSIsInN1YiI6IjYyOWIwNGVmYTQ0ZDA5NTI3NmVjZDA4ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2HYS4gkTpgn-R7S0GVqvRXWQl4lMaMYcQbo1EQ5DNYI';
@@ -37,11 +40,17 @@ class _HomePageState extends State<HomePage> {
     Map trendingResult = await tmdbWithCustomLogs.v3.trending.getTrending();
     Map topRatedResult = await tmdbWithCustomLogs.v3.movies.getTopRated();
     Map tvResult = await tmdbWithCustomLogs.v3.tv.getPopular();
+    Map tvAirResult = await tmdbWithCustomLogs.v3.tv.getOnTheAir();
+    Map movieDiscoverResult = await tmdbWithCustomLogs.v3.discover.getMovies();
+    Map movieUpcomingResult = await tmdbWithCustomLogs.v3.movies.getUpcoming();
 
     setState(() {
       trendingMovies = trendingResult['results'];
       topRatedMovies = topRatedResult['results'];
       tv = tvResult['results'];
+      tvAir = tvAirResult['results'];
+      movieDiscover = movieDiscoverResult['results'];
+      movieUpcoming = movieUpcomingResult['results'];
     });
 
     print(tv);
@@ -73,10 +82,25 @@ class _HomePageState extends State<HomePage> {
             title: 'title',
           ),
           Carousal(
+            trending: movieUpcoming,
+            heading: 'Upcoming Movies',
+            title: 'title',
+          ),
+          Carousal(
             trending: tv,
             heading: 'Popular TV',
             title: 'original_name',
           ),
+          Carousal(
+            trending: tvAir,
+            heading: 'TV On Air',
+            title: 'original_name',
+          ),
+          // Carousal(
+          //   trending: movieDiscover,
+          //   heading: 'Discover',
+          //   title: 'title',
+          // ),
         ],
       ),
     );
